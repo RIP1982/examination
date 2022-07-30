9835b7ae0bdb3ed2dd613dc91d0a285c91087c84
RIP1982 <perovsky@list.ru>
2022-07-30 12:53:17 +0400

    create string array

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.Clear();
+
+Console.Write("Введите длину массива строк, не более 10: ");
+
+int size;
+
+if (int.TryParse(Console.ReadLine(), out size) && size > 0 && size <= 10)
+{
+    Console.WriteLine("Заполните массив строками!");
+}
+else
+{
+    Console.WriteLine("Введены не допустимые символы!");
+    return;
+}
+
+string[] strArray = new string[size];
